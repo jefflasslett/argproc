f5a5adfc9370c9a027f7912167b25fd849b0046d
Jeff Lasslett <jeff.lasslett@gmail.com>
2013-06-25 15:26:42 +1000

    initial commit

---
+++ Main.hs
@@ -0,0 +1,82 @@
+module Main where
+
+import System.Console.GetOpt
+import System.Environment
+import System.Exit
+import Control.Monad
+import System.IO
+import Data.Char
+import Data.List
+
+version :: String
+version = "0.1.0"
+
+{- These optional arguments could easily be Strings.  GetOpt handles their
+ - optional nature just fine.  I like having them as Maybe values though.
+ -}
+data Options = Options { optOption      ::Maybe String
+                       , optArgument    ::Maybe String
+                       }
+
+defaultOptions :: Options
+defaultOptions = Options { optOption   = Nothing
+                         , optArgument = Nothing
+                         }
+{-
+ReqArg
+  (String -> a)  -- Usual ArgDescr for Required arg. (The higher order function bit anyway)
+
+When 'a' is Option -> IO Option then, substituting for 'a', we get:
+
+String -> Option -> IO Option
+
+Voila!!
+-}
+
+optionDescriptions :: [ OptDescr ( Options ->IO Options ) ]
+optionDescriptions =
+  [ Option "o" [ "option" ]
+      ( OptArg ( \arg opt -> return $ opt { optOption = arg } ) "" )
+      "Set this optional command-line option"
+
+  , Option "c" [ "carrier" ]
+      ( OptArg ( \arg opt -> return opt { optArgument = arg } ) "" )
+      "Give this hypothetical command-line argument"
+
+  , Option "v" [ "version" ]
+      ( NoArg ( \_ -> ( putStrLn $ "This is haskell arg processor v" ++ version ) >>  exitWith ExitSuccess ) )
+      "Print version information"
+
+  , Option "h" [ "help" ]
+      ( NoArg ( \_ -> ( putStrLn $ usageInfo "argproc" optionDescriptions ) >>  exitWith ExitSuccess ) )
+      "Print this usage information"
+  ]
+
+printArgs :: Options -> IO ()
+printArgs opts =
+  do
+    case optOption opts of
+      Just s -> putStrLn $ "Option: " ++ s
+      _      -> putStrLn $ "Option not given"
+    case optArgument opts of
+      Just s -> putStrLn $ "Argument: " ++ s
+      _      -> putStrLn $ "Argument not given"
+
+main = 
+  do
+    args <- getArgs
+ 
+    -- Parse options, getting a list of option actions
+    let (actions, nonOptions, errors) = getOpt RequireOrder optionDescriptions args
+ 
+    -- Here we thread startOptions through all supplied option actions
+    opts <- foldl (>>=) (return defaultOptions) actions
+
+    printArgs opts
+    putStrLn "Good-bye!"
+
+ 
+
+
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# argproc
+
+This is a small Haskell program that shows one way to process command-line
+arguments. I was guided by the information and code at 
+<http://www.haskell.org/haskellwiki/High-level_option_handling_with_GetOpt>
+
+This program is probably only helpful for haskell noobs like myself.
+
+## Compiling
+
+`$ ghc Main.hs -o argproc`
+
