2840ceb54079b4e26f62e9e7b43b6e303078b836
Jeff Lasslett <jeff.lasslett@gmail.com>
2013-07-01 16:13:14 +1000

    Improve the explanation of optionDescriptions list

--- Main.hs
+++ Main.hs
@@ -18,21 +18,52 @@ data Options = Options { optOption      ::Maybe String
                        , optArgument    ::Maybe String
                        }
 
+-- The defaults for the parameters
 defaultOptions :: Options
 defaultOptions = Options { optOption   = Nothing
                          , optArgument = Nothing
                          }
 {-
-ReqArg
-  (String -> a)  -- Usual ArgDescr for Required arg. (The higher order function bit anyway)
-
-When 'a' is Option -> IO Option then, substituting for 'a', we get:
-
-String -> Option -> IO Option
-
-Voila!!
--}
-
+ - Describes the valid options for the app. An array of 
+ - OptDescr a, where 'a' is (Option -> IO Option).
+ -
+ - Each element of the array has four components that describe a single option:
+ -
+ - The arguments to Option (the OptDescr constructor) are:
+ -
+ -  * list of short option characters
+ -  * list of long option strings (without "--")
+ -  * argument descriptor
+ -  * explanation of option for user
+ -
+ - Option [Char] [String] (ArgDescr a) String	 
+ -
+ - The ArgDescr (argument descriptor) is interesting in this case.
+ - Note that the ReqArg and OptArg constructors take functions as
+ - arguments: (String -> a) and (Maybe String -> a).
+ - 
+ - Here is the ArgDescr date type:
+ -
+      data ArgDescr a
+        = NoArg                   a         -- no argument expected
+        | ReqArg (String       -> a) String -- option requires argument
+        | OptArg (Maybe String -> a) String -- optional argument
+ -
+ - For example, look at the ReqArg constructor of ArgDescr:
+ -
+        ReqArg (String -> a) String
+ -
+ - Recall that in this example 'a' is of type (Options -> IO Options).
+ -
+ - Substituting (Options -> IO Options) for 'a' in (String -> a) 
+ - we get ...
+ -
+    String -> (Options -> IO Options ) which is just
+    String -> Options -> IO Option
+ -
+ - Now the types of the lambdas in the List of OptDescr below 
+ - should make sense.
+ -}
 optionDescriptions :: [ OptDescr ( Options ->IO Options ) ]
 optionDescriptions =
   [ Option "o" [ "option" ]
